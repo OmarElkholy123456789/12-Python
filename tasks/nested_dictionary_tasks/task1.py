c4cea929e73f8281f621fbfc51b808dc861f680d
OmarElkholy123456789 <oe22097@my.westlake.school.nz>
2025-05-19 09:35:18 +1200

    Worked on nested dictionary games task.

---
+++ tasks/nested_dictionary_tasks/task1.py
@@ -0,0 +1,76 @@
+import easygui
+
+favourite_games = {
+    1 : {
+        "Title" : "Fortnite",
+        "Genre" : "Survival, battle royale, sandbox",
+        "Platform(s)" : "PC, Xbox, PS, Nintendo Switch",
+        "Release Year" : 2017,
+        "Rating out of 10" : 9.6
+    },
+    2 : {
+        "Title" : "GTA V",
+        "Genre" : "Action Adventure",
+        "Platform(s)" : "PC, Xbox, PS",
+        "Release Year" : 2013,
+        "Rating out of 10" : 9.5
+    },
+    3 : {
+        "Title" : "Minecraft",
+        "Genre" : "Survival, sandbox, adventure",
+        "Platform(s)" : "PC, Xbox, PS, Nintendo Switch, Mobile",
+        "Release Year" : int(),
+        "Rating out of 10" : float()    
+    },
+    4 : {
+        "Title" : "Rainbow Six Seige",
+        "Genre" : "Shooter, Search and Destroy",
+        "Platform(s)" : "PC, Xbox PS",
+        "Release Year" : 2015,
+        "Rating out of 10" : 9.3
+    },
+    5 : {
+        "Title" : "Rocket League",
+        "Genre" : "Sports",
+        "Platform(s)" : "PC, Xbox, PC, Nintendo Switch",
+        "Release Year" : 2015,
+        "Rating out of 10" : 8.8 
+    },
+    6 : {
+        "Title" : "Skylanders",
+        "Genre" : "Adventure, puzzle",
+        "Platform(s)" : "Xbox, PS, Nintendo Switch, Wii",
+        "Release Year" : 2011,
+        "Rating out of 10" : 8.6   
+    },
+    7 : {
+        "Title" : "Fifa",
+        "Genre" : "Sports",
+        "Platform(s)" : "PC, Xbox, PS",
+        "Release Year" : 1993,
+        "Rating out of 10" : 8.5 
+    },
+    8 : {
+        "Title" : "NBA 2K",
+        "Genre" : "Sports",
+        "Platform(s)" : "PC, Xbox, PS, Nintendo Switch",
+        "Release Year" : 1999,
+        "Rating out of 10" : 8.2
+    },
+    9 : {
+        "Title" : "Roblox",
+        "Genre" : "Simulation, multiplayer, action",
+        "Platform(s)" : "PC, Xbox, PS, Nintendo Switch, Mobile",
+        "Release Year" : 2006,
+        "Rating out of 10" : 8.0
+    },
+    10 : {
+        "Title" : "Clash Royale",
+        "Genre" : "Tower defence, multiplayer, strategy",
+        "Platform(s)" : "Mobile",
+        "Release Year" : 2016,
+        "Rating out of 10" : 7.8
+    }
+}
+
+for rank, key, value 
