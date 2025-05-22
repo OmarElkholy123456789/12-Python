05058f1690f99428ba9498e0e7ea7dfc8837657b
OmarElkholy123456789 <oe22097@my.westlake.school.nz>
2025-05-22 12:49:39 +1200

    Made more progress on the top 10 games program.

--- tasks/nested_dictionary_tasks/task1.py
+++ tasks/nested_dictionary_tasks/task1.py
@@ -1,70 +1,70 @@
 import easygui
 
 favourite_games = {
-    1 : {
+    "Fortnite" : {
         "Title" : "Fortnite",
         "Genre" : "Survival, battle royale, sandbox",
         "Platform(s)" : "PC, Xbox, PS, Nintendo Switch",
         "Release Year" : 2017,
         "Rating out of 10" : 9.6
     },
-    2 : {
+    "GTA V" : {
         "Title" : "GTA V",
         "Genre" : "Action Adventure",
         "Platform(s)" : "PC, Xbox, PS",
         "Release Year" : 2013,
         "Rating out of 10" : 9.5
     },
-    3 : {
+    "Minecraft" : {
         "Title" : "Minecraft",
         "Genre" : "Survival, sandbox, adventure",
         "Platform(s)" : "PC, Xbox, PS, Nintendo Switch, Mobile",
         "Release Year" : 2009,
         "Rating out of 10" : 9.4
     },
-    4 : {
+    "Rainbow Six Seige" : {
         "Title" : "Rainbow Six Seige",
         "Genre" : "Shooter, Search and Destroy",
         "Platform(s)" : "PC, Xbox PS",
         "Release Year" : 2015,
         "Rating out of 10" : 9.3
     },
-    5 : {
+    "Rocket League" : {
         "Title" : "Rocket League",
         "Genre" : "Sports",
         "Platform(s)" : "PC, Xbox, PC, Nintendo Switch",
         "Release Year" : 2015,
         "Rating out of 10" : 8.8 
     },
-    6 : {
+    "Skylanders" : {
         "Title" : "Skylanders",
         "Genre" : "Adventure, puzzle",
         "Platform(s)" : "Xbox, PS, Nintendo Switch, Wii",
         "Release Year" : 2011,
         "Rating out of 10" : 8.6   
     },
-    7 : {
+    "Fifa" : {
         "Title" : "Fifa",
         "Genre" : "Sports",
         "Platform(s)" : "PC, Xbox, PS",
         "Release Year" : 1993,
         "Rating out of 10" : 8.5 
     },
-    8 : {
+    "NBA 2K" : {
         "Title" : "NBA 2K",
         "Genre" : "Sports",
         "Platform(s)" : "PC, Xbox, PS, Nintendo Switch",
         "Release Year" : 1999,
         "Rating out of 10" : 8.2
     },
-    9 : {
+    "Roblox" : {
         "Title" : "Roblox",
         "Genre" : "Simulation, multiplayer, action",
         "Platform(s)" : "PC, Xbox, PS, Nintendo Switch, Mobile",
         "Release Year" : 2006,
         "Rating out of 10" : 8.0
     },
-    10 : {
+    "Clash Royale" : {
         "Title" : "Clash Royale",
         "Genre" : "Tower defence, multiplayer, strategy",
         "Platform(s)" : "Mobile",
@@ -90,7 +90,7 @@ def show_menu():
     options = {
         #"Add Game" : add_game,
         "Edit Game" : edit_game,
-        "Quit Menu" : quit_menu
+        #"Quit Menu" : quit_menu
     }
 
     choice_list = []
@@ -102,3 +102,30 @@ def show_menu():
     choices = choice_list)
 
     function = options[user_choice]()
+
+def edit_game():
+    """
+    """
+    game_titles = []
+
+    for key, value in favourite_games.items():
+        game_titles.append(value["Title"])
+
+    user_choice = easygui.choicebox("What game would you like to edit?", \
+    choices = game_titles)
+
+    user_options = []
+
+    for key in favourite_games.items():
+        user_options.append(key)
+
+    user_info = easygui.buttonbox("What would you like to edit?", \
+    choices = user_options)
+
+    
+
+
+
+
+
+show_menu()
