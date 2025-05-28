4a5de7f35a41afd42cfba4c8d755e4615876de15
OmarElkholy123456789 <oe22097@my.westlake.school.nz>
2025-05-28 15:08:25 +1200

    Finished top 10 games python project.

--- tasks/nested_dictionary_tasks/task1.py
+++ tasks/nested_dictionary_tasks/task1.py
@@ -73,25 +73,15 @@ favourite_games = {
     }
 }
 
-def print_favourite_games():
-    for rank, game_info in favourite_games.items():
-        msg = f"Rank: {rank}\n"
-        msg += f"Title: {game_info['Title']}\n"
-        msg += f"Genre: {game_info['Genre']}\n"
-        msg += f"Platform: {game_info['Platform(s)']}\n"
-        msg += f"Release Year: {game_info['Release Year']}\n"
-        msg += f"Rating: {game_info['Rating out of 10']}/10"
-        
-        easygui.msgbox(msg, title=f"Favourite Game #{rank}")
-
 def show_menu():
     """
     """
     options = {
-        print_games 
-        #"Add Game" : add_game,
+        "Print Games" : print_favourite_games,
+        "Add Game" : add_game,
         "Edit Game" : edit_game,
-        #"Quit Menu" : quit_menu
+        "Quit Menu" : quit,
+        "Delete Game" : delete_game
     }
 
     choice_list = []
@@ -103,6 +93,17 @@ def show_menu():
     choices = choice_list)
 
     function = options[user_choice]()
+
+def print_favourite_games():
+    for rank, game_info in favourite_games.items():
+        msg = f"Rank: {rank}\n"
+        msg += f"Title: {game_info['Title']}\n"
+        msg += f"Genre: {game_info['Genre']}\n"
+        msg += f"Platform: {game_info['Platform(s)']}\n"
+        msg += f"Release Year: {game_info['Release Year']}\n"
+        msg += f"Rating: {game_info['Rating out of 10']}/10"
+        
+        easygui.msgbox(msg, title=f"Favourite Game #{rank}")
 
 def edit_game():
     """
@@ -134,10 +135,38 @@ def edit_game():
 
     show_menu()
 
+def add_game():
+    game_name = easygui.enterbox(f"Please enter the name of the game")
+    game_genre = easygui.enterbox(f"Please enter the genre for {game_name}")
+    game_platform = easygui.enterbox(f"Please enter the platform(s) for {game_name}")
+    game_release_year = easygui.enterbox(f"Please enter the release year of {game_name}")
+    game_rating = easygui.enterbox(f"Please enter the rating for {game_name}")
+
+    favourite_games[game_name] = {
+        "Title" : game_name,
+        "Genre" : game_genre,
+        "Platform(s)" : game_platform,
+        "Release Year" : game_release_year,
+        "Rating out of 10" : game_rating
+    }
     
+    show_menu()
 
+def delete_game():
 
+    game_titles = []
 
+    for key, titles in favourite_games.items():
+        game_titles.append(titles["Title"])
 
+    deleted_games = easygui.buttonbox("What game would you like to delete", \
+     choices = game_titles)
+    
+    favourite_games.pop(deleted_games)
+
+    show_menu()
+
+def quit():
+    quit
 
 show_menu()
